b45895e41809f9a65c806264f4965ec2ba66fde2
LewisBrogan <contact@lewisbrogan.co.uk>
2024-01-21 02:27:35 +0000

    Updated bot.py main

--- bot/bot.py
+++ bot/bot.py
@@ -1,20 +1,14 @@
 from typing import Any
 import discord
 import os
+from discord import Intents
 
 class MyBot(discord.Client):
     """A custom Discord bot class."""
 
     async def on_ready(self):
-        """Print a message and announce in a specific channel that we have successfully logged in."""
+        """Print a message indicating that we have successfully logged in."""
         print(f"We have logged in as {self.user}")
-
-        channel_id = 1198411010512535653
-        channel = self.get_channel(channel_id)
-        if channel:
-            await channel.send('The bot has started up!')
-        else:
-            print(f"Could not find channel with ID {channel_id}")
 
     async def on_message(self, message: discord.Message):
         """Respond to messages starting with '$hello'."""
@@ -30,7 +24,10 @@ def run_bot():
     if token is None:
         raise ValueError("Missing DISCORD_TOKEN environment variable.")
 
-    bot = MyBot()
+    intents = Intents.default()
+    intents.messages = True
+
+    bot = MyBot(intents=intents)
     bot.run(token)
 
 if __name__ == "__main__":
