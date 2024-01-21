69f91eadb43f9c9bef9be20ea4bf8f06bbbb33d9
LewisBrogan <contact@lewisbrogan.co.uk>
2024-01-21 04:14:32 +0000

    Updated bot commands for slash

--- bot/bot.py
+++ bot/bot.py
@@ -1,22 +1,27 @@
-import discord
 import os
-
+import discord
 from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
+from interactions import CommandContext, SlashCommand
 from utils.docker_utils import get_docker_status
 
 class MyBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix="!", intents=discord.Intents.default())
-        self.slash = SlashCommand(self, sync_commands=True)
 
     async def on_ready(self):
-        channel_id = 1198411010512535653
+        print(f"Logged in as {self.user.name}")
+        await self.change_presence(activity=discord.Game(name="Handling Slash Commands"))
+
+        channel_id = 1198411010512535653 
         channel = self.get_channel(channel_id)
         if channel:
             await channel.send(f'{self.user} has logged in!')
         else:
             print(f"Could not find channel with ID {channel_id}")
+
+    async def dockerstatus(self, ctx: CommandContext):
+        docker_status = get_docker_status()
+        await ctx.send(f"```{docker_status}```")
 
 def run_bot():
     token = os.getenv('DISCORD_TOKEN')
@@ -24,12 +29,7 @@ def run_bot():
         raise ValueError("Missing DISCORD_TOKEN environment variable.")
 
     bot = MyBot()
-
-    @bot.slash.slash(name="dockerstatus", description="Get Docker status")
-    async def dockerstatus(ctx: SlashContext):
-        docker_status = get_docker_status()
-        await ctx.send(f"```{docker_status}```")
-
+    bot.add_application_command(SlashCommand(bot.dockerstatus, "dockerstatus", "Get Docker status"))
     bot.run(token)
 
 if __name__ == "__main__":
