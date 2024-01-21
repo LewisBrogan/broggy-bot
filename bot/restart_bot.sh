6826befdd468ff774cb4ba91b79225dc18dd43b2
LewisBrogan <contact@lewisbrogan.co.uk>
2024-01-21 01:47:37 +0000

    Added workflow updates

---
+++ bot/restart_bot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /home/ubuntu/Docker/broggy-discord-bot
+
+if pgrep -f bot.py; then
+    echo "Bot is running. Stopping bot..."
+    pkill -f bot.py
+    sleep 5
+else
+    echo "Bot is not running."
+fi
+
+echo "Pulling latest code..."
+git pull
+
+echo "Starting bot..."
+nohup python3 bot/bot.py &
+echo "Bot started."
